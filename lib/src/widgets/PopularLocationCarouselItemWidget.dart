78426ace5b1a64bdb4bc181211e80c6532f34a15
yustikodm <yustiko404@gmail.com>
2021-05-21 17:21:16 +0700

    update

--- lib/src/widgets/PopularLocationCarouselItemWidget.dart
+++ lib/src/widgets/PopularLocationCarouselItemWidget.dart
@@ -1,4 +1,5 @@
 import 'package:Unio/src/models/university.dart';
+import 'package:Unio/src/utilities/global.dart';
 
 import '../models/utilities.dart';
 import '../models/route_argument.dart';
@@ -6,7 +7,7 @@ import '../models/route_argument.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
-class PopularLocationCarouselItemWidget extends StatelessWidget {
+class PopularLocationCarouselItemWidget extends StatefulWidget {
   String heroTag;
   double marginLeft;
   University university;
@@ -19,21 +20,66 @@ class PopularLocationCarouselItemWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _PopularLocationCarouselItemWidgetState createState() =>
+      _PopularLocationCarouselItemWidgetState();
+}
+
+class _PopularLocationCarouselItemWidgetState
+    extends State<PopularLocationCarouselItemWidget> {
+  Future<void> _showNeedLoginAlert(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('You are not logged in!'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Are you wanna login first?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/SignIn');
+              },
+            ),
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        print(university.universityId is int);
-        Navigator.of(context).pushNamed('/Detail',
-            arguments: RouteArgument(
-                param1: university.universityId, param2: 'universities'));
+        print(widget.university.universityId is int);
+        if (Global.instance.apiToken != null) {
+          Navigator.of(context).pushNamed('/Detail',
+              arguments: RouteArgument(
+                  param1: widget.university.universityId,
+                  param2: 'universities'));
+        } else {
+          _showNeedLoginAlert(context);
+        }
       },
       child: Container(
-        margin: EdgeInsets.only(left: this.marginLeft, right: 20),
+        margin: EdgeInsets.only(left: this.widget.marginLeft, right: 20),
         child: Stack(
           alignment: AlignmentDirectional.topCenter,
           children: <Widget>[
             Hero(
-              tag: heroTag + university.id,
+              tag: widget.heroTag + widget.university.id,
               child: Container(
                 width: 180,
                 height: 185,
@@ -42,8 +88,8 @@ class PopularLocationCarouselItemWidget extends StatelessWidget {
                   image: DecorationImage(
                     fit: BoxFit.cover,
                     // image: AssetImage(utilitie.image),
-                    image: (this.heroTag == "home_flash_sales")
-                        ? AssetImage(university.logo)
+                    image: (this.widget.heroTag == "home_flash_sales")
+                        ? AssetImage(widget.university.logo)
                         : NetworkImage(
                             "https://i.ibb.co/RgwbsQy/icon-campus.jpg"),
                   ),
@@ -65,7 +111,7 @@ class PopularLocationCarouselItemWidget extends StatelessWidget {
                   ]),
               child: Center(
                 child: Text(
-                  university.name,
+                  widget.university.name,
                   style: Theme.of(context).textTheme.body2,
                   maxLines: 1,
                   softWrap: false,
